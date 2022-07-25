97fbf11e5e8fbe92690bd9c7fdcbae1e52fee936
Artur <arturaitmuratov@gmail.com>
2022-07-25 18:22:12 +0500

    Create 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cs

---
+++ Medium/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cs
@@ -0,0 +1,19 @@
+//Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+//If target is not found in the array, return [-1, -1].
+
+//You must write an algorithm with O(log n) runtime complexity.
+
+
+public class Solution {
+    public int[] SearchRange(int[] nums, int target) {
+        int[] res;
+        int first_index = Array.IndexOf(nums, target);
+        int last_index = Array.LastIndexOf(nums, target);
+        if (first_index > -1 && last_index > -1)
+            res = new int[]{ first_index,  last_index };
+        else
+            res = new int[] { first_index,  last_index };
+        return res;
+    }
+}
